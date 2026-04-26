f2edd847716e3af5a22cffc480d49f6d8fa94c8f
jyk3716 <84884345+jyk3716@users.noreply.github.com>
2026-04-26 17:51:54 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ Java/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] solution(int[] num_list) {
+        // num_list와 동일한 길이의 answer 배열 생성
         int[] answer = new int[num_list.length];
+        // answer 배열의 끝부터 원소 대입
         for (int i=0; i<num_list.length; i++) {
             answer[num_list.length-1-i] = num_list[i];
         }
