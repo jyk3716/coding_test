1a183a073673e9aa0ce1c7956ca24acb4d7a7820
jyk3716 <84884345+jyk3716@users.noreply.github.com>
2026-04-26 18:44:39 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.08 ms, Memory: 85.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,9 +1,16 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for (int i = my_string.length() - 1; i >= 0; i--) {
-            answer += my_string.charAt(i);
-        }
+        // charAt 메소드 사용하기
+        // 성능 8.44 ~ 11.36ms
+        // String answer = "";
+        // for (int i = my_string.length() - 1; i >= 0; i--) {
+        //     answer += my_string.charAt(i);
+        // }
+        
+        // StringBuffer / reverse 메소드 사용하기
+        // 성능 0.06 ~ 0.08ms
+        StringBuffer sb = new StringBuffer(my_string);        
+        String answer = sb.reverse().toString();
         return answer;
     }
 }
