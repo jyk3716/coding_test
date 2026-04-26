0f8c4dbddd905967329c4dc9d5157e163feacbb5
jyk3716 <84884345+jyk3716@users.noreply.github.com>
2026-04-26 18:40:18 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.44 ms, Memory: 75.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ Java/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,7 +1,10 @@
 class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
+        // 향상된 for문과 split("")을 활용해 
+        // 한 글자씩 쪼개고 스트링 배열 만듦
         for (String s : my_string.split("")) {
+            // repeat() 활용해 각 글자를 n번 반복
             answer += s.repeat(n);
         }
         return answer;
